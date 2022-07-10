1f9fdefe2a3b616db88f52c2519220a948ee9fcb
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2022-07-10 10:28:10 -0300

    Implemented Adapter and RecyclerView to Java MainActivity

--- app/src/main/java/com/example/simulator/ui/MainActivity.java
+++ app/src/main/java/com/example/simulator/ui/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.simulator.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.simulator.R;
+import com.example.simulator.data.MatchesApi;
 import com.example.simulator.databinding.ActivityMainBinding;
+import com.example.simulator.domain.Match;
+import com.example.simulator.ui.adapter.MatchesAdapter;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private MatchesApi matchesApi;
+    private RecyclerView.Adapter matchesAdapter;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,13 +37,40 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        setupHttpClient();
         setupMatchesList();
         setupMatchesRefresh();
         setupFloatingActionButton();
     }
 
+    private void setupHttpClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://adenilsonricardo.github.io/matches-simulator-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        matchesApi = retrofit.create(MatchesApi.class);
+    }
+
     private void setupMatchesList() {
-        // TODO: Listar as partidas, consumindo nossa API.
+        binding.rvMatches.setHasFixedSize(true);
+        binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
+            @Override
+            public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+                if (response.isSuccessful()) {
+                    List<Match> matches = response.body();
+                    matchesAdapter = new MatchesAdapter(matches);
+                    binding.rvMatches.setAdapter(matchesAdapter);
+                } else {
+                    showErrorMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Match>> call, Throwable t) {
+                showErrorMessage();
+            }
+        });
     }
 
     private void setupMatchesRefresh() {
@@ -34,6 +80,11 @@ public class MainActivity extends AppCompatActivity {
     private void setupFloatingActionButton() {
         //TODO: Criar evento de click e simulação de partida.
     }
+
+    private void showErrorMessage() {
+        Snackbar.make(binding.fabSimulate, R.string.error_api, Snackbar.LENGTH_LONG).show();
+    }
+
 
 
 }
